fc5025bc12e49595f5a53d99dc7342d23ec6c3bc
Jiří Stránský <jistr@jistr.com>
2022-10-28 18:40:46 +0200

    Upgrade to CentOS Stream 8

--- build.sh
+++ build.sh
@@ -5,13 +5,20 @@ set -euxo pipefail
 AWL_REF=r0.62
 DAVICAL_REF=r1.1.10
 
+dnf config-manager --set-enabled powertools
+
 # CentOS image comes cleaned of locales, reinstall them
-yum -y reinstall glibc-common
-yum -y install epel-release
+dnf -y reinstall glibc-common
+dnf -y install epel-release
 # postgresql package is installed because of psql client
-# (it doesn't # install the server)
-yum -y install gettext git httpd perl-YAML perl-YAML-LibYAML perl-DBD-Pg perl-DBI php php-curl php-imap php-ldap php-pgsql php-xml php-pear-Net-Curl postgresql
-yum clean all
+# (it doesn't install the server)
+dnf -y install gettext git httpd perl-YAML perl-YAML-LibYAML perl-DBD-Pg perl-DBI php php-curl php-ldap php-pgsql php-xml postgresql
+dnf clean all
+
+# Use prefork for php instead of the default event MPM
+cat > /etc/httpd/conf.modules.d/00-mpm.conf <<EOF
+LoadModule mpm_prefork_module modules/mod_mpm_prefork.so
+EOF
 
 pushd /var/www/html
 git clone --branch $AWL_REF https://gitlab.com/davical-project/awl.git
